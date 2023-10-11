9cd74f1cb96b23c15d10b5a3e11f8112b010dfb4
Sahilgupta942 <61141032+Sahilgupta942@users.noreply.github.com>
2023-10-11 18:13:18 +0530

    Update CreateInstance.tf

--- CreateInstance.tf
+++ CreateInstance.tf
@@ -4,6 +4,11 @@
 #    region = "us-east-1"
 #}
 resource "aws_instance" "Test_instance1" {
+    count = 2
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
+
+    tags{
+        Name = "TwoDemoInstances"
+    }
 }
