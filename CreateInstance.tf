a9ce5c5f116120bc54f1574e2b3778ca2462a41c
Sahilgupta942 <61141032+Sahilgupta942@users.noreply.github.com>
2023-10-11 18:18:55 +0530

    Update CreateInstance.tf

--- CreateInstance.tf
+++ CreateInstance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "Test_instance1" {
     count = 2
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
-
+#Assign unique name to each instance launched under count
     tags = {
         Name = "DemoInstance-${count.index}"
     }
