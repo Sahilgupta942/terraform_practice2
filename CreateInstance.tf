6e7343e3141b1459dd135b9596d030eb6886f85c
Sahilgupta942 <61141032+Sahilgupta942@users.noreply.github.com>
2023-10-11 16:08:16 +0530

    Create CreateInstance.tf

---
+++ CreateInstance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    access_key = "AKIA4RMMTCZVVDMSQI4R"
+    secret_key = "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"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "Test_instance1" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+}
